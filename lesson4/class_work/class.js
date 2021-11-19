dfabddb73f18ab592378fa36824e71ef84347e1a
OlgaBuchko <lyolyabo92@gmail.com>
2021-11-19 18:27:03 +0200

    проа

--- lesson4/class_work/class.js
+++ lesson4/class_work/class.js
@@ -1,60 +1,60 @@
 
 // - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-function min(a,b,c) {
-  if (a<b &&a<c){
-      console.log(a)
-  }  else if(b<c &&b<a){
-      console.log(b)
-  }else if(
-      c<a &&c<b){
-      console.log(c)
-  }
-}
-min(1,3,8);
+// function min(a,b,c) {
+//   if (a<b &&a<c){
+//       console.log(a)
+//   }  else if(b<c &&b<a){
+//       console.log(b)
+//   }else if(
+//       c<a &&c<b){
+//       console.log(c)
+//   }
+// }
+// min(1,3,8);
 // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
-function max(a,b,c) {
-    if (a>b && a>c){
-        console.log(a)
-    }  else if(b>c &&b>a){
-        console.log(b)
-    }else if (
-        c>a && c>b
-    )
-
-        console.log(c)
-
-}
-max(6,1,8);
+// function max(a,b,c) {
+//     if (a>b && a>c){
+//         console.log(a)
+//     }  else if(b>c &&b>a){
+//         console.log(b)
+//     }else if (
+//         c>a && c>b
+//     )
+//
+//         console.log(c)
+//
+// }
+// max(6,1,8);
 
 // - створити функцію яка повертає найбільше число з масиву
 let number =[3,5,44,3,2,664,5,87]
-function maxArr(arr)
-{
-let max = [0];
-    for (const numb of arr) {
-        if (numb > max) {
-            max = numb
-        }
-    }
-   return max;
-}
+// function maxArr(arr)
+// {
+// let max = [0];
+//     for (const numb of arr) {
+//         if (numb > max) {
+//             max = numb
+//         }
+//     }
+//    return max;
+// }
 // maxArr(number)
 //
 
 
 
 // - створити функцію яка повертає найменьше число з масиву
-function minArr(arr)
-{
-    let min = arr[0];
-    for (const numb of arr) {
-
-        if (numb < min) {
-            min = numb
-        }
-    }
-   return min;
-}
+// function minArr(arr)
+// {
+//     let min = arr[0];
+//     for (const numb of arr) {
+//
+//         if (numb < min) {
+//             min = numb
+//         }
+//     }
+//    return min;
+// }
 // minArr(number)
 
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
@@ -76,15 +76,15 @@ function minArr(arr)
 // }
 // averageSum(number)
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-function minmax(arr)
-{
-    let  mini = minArr(arr);
-
-   let maxi =  maxArr(arr);
-    console.log(maxi);
-    return mini;
-}
-minmax(number)
+// function minmax(arr)
+// {
+//     let  mini = minArr(arr);
+//
+//    let maxi =  maxArr(arr);
+//     console.log(maxi);
+//     return mini;
+// }
+// minmax(number)
 
 // - створити функцію яка заповнює масив рандомними числами
 // let arrey8 =[];
@@ -114,10 +114,9 @@ minmax(number)
 function startEnd(arr5) {
     let array2 = [];
 
-for (let i = arr5.length-1, j=0; i >= 0; i=i-1) {
-    let array = arr5[i];j++;
+for (let i = arr5.length-1, j=0; i >= 0; i=i-1, j++) {
 
-array2[j]=array;
+array2[j]=arr5[i];
 
     }
     console.log(array2)
