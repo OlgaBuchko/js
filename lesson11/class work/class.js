2e007b34434aeb1d3a00b6ad435e37ac7f531c81
OlgaBuchko <lyolyabo92@gmail.com>
2021-12-13 10:21:36 +0200

    df

---
+++ lesson11/class work/class.js
@@ -0,0 +1,31 @@
+// є масив -
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+// створити під кожен об'єкт свій блок з конопкою "додати до улюблених" при натисканні на яку об'єкт потрапляє до масиву favorites улюблених обраних об'єктів в локальному сховищі.
+// Створити сторніку favorites.html при переході на яку потрібно вивест в документ всіх обраних на попередньому етапі.
+let users_favorites = [];
+
+for (const user of users) {
+    let div = document.createElement('div');
+    document.body.appendChild(div);
+    div.innerText= JSON.stringify(user);
+    let button = document.createElement('button');
+    div.appendChild(button);
+    button.innerText = 'додати до улюблених';
+    button.onclick = function () {
+        users_favorites.push(user)
+        console.log(users_favorites)
+        localStorage.setItem('favorites',JSON.stringify( users_favorites))
+    }
+}
